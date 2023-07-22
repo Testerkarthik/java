2180311c0e7e95da804a1c106a2d7e8fe5bca90f
TesterKarthik <karthikkrish426@gmail.com>
2023-07-22 13:58:44 +0530

    karthik

---
+++ src/org/sample/Sample.java
@@ -0,0 +1,42 @@
+package org.sample;
+
+
+public class Sample {
+	
+  public static void main(String[] args) {
+	
+	  String l = "karthikkrishnan";
+	  
+	  if (l.contains("k")) {
+		  System.out.println("true");
+		  
+		
+	}
+	  else {
+		System.out.println("false");
+	}
+		
+  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+  }
+  }
+  
+
+	
+	
+
